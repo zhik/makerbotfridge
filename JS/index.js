934ebaacb2b61760d9e727c94aaa0281248c08d0
Joseph Babbitt <jrebbabbitt@gmail.com>
2015-06-14 13:19:33 -0400

    Fixed system breaking functions

--- JS/index.js
+++ JS/index.js
@@ -1,7 +1,4 @@
-$( document ).ready(function() {
-    console.log( "This is a console log test." );
-    
-    //set up the two FireBase vars (disabled due to system braking bug caused by overwrite)
+  //set up the two FireBase vars (disabled due to system braking bug caused by overwrite)
 			//ref.set({
 			//	unlocked: null,
 			//	closed: null,
@@ -13,26 +10,7 @@ $( document ).ready(function() {
 			var closed = '';
 			var ref = new Firebase("https://fridgelock.firebaseio.com");
 			
-							ref.child('unlocked').on("value", function(snapshot) {
-   unlocked = snapshot.val();
-   console.log(unlocked);
-   
-if (unlocked == false) {var unlockedmessage = 'Locked' }
-else if (unlocked == true) {var unlockedmessage = 'Unlocked'}
-   document.getElementById("unlocked").innerHTML = unlockedmessage;
-}, function (errorObject) {
-  console.log("The read failed: " + errorObject.code);
-});
-
-			ref.child('closed').on("value", 
-function(snapshot) {
-   closed = snapshot.val();
-   console.log(closed);
-}, function (errorObject) {
-  console.log("The read failed: " + errorObject.code);
-});
-
-
+			
 			// function that will send lockdoor signal, console.log is for debuging
 			function lockdoor() {
 				ref.update({
@@ -45,6 +23,40 @@ function(snapshot) {
 				unlocked: true,
 			})	
 			}
+
+
+
+$( document ).ready(function() {
+    console.log( "This is a console log test." );
+ 							ref.child('unlocked').on("value", function(snapshot) {
+   unlocked = snapshot.val();
+   console.log(unlocked);
+   
+if (unlocked == false) {var unlockedmessage = 'Locked' }
+else if (unlocked == true) {var unlockedmessage = 'Unlocked'}
+else {'everything broke send help'};
+   document.getElementById("unlocked").innerHTML = unlockedmessage;
+   
+}, function (errorObject) {
+  console.log("The read failed: " + errorObject.code);
+});
+
+			ref.child('closed').on("value", 
+function(snapshot) {
+   closed = snapshot.val();
+   console.log(closed);
+   
+   if (closed == true) {var closedmessage = 'Closed' }
+else if (closed == false) {var closedmessage = 'Open'}
+else {'everything broke send help'};
+   document.getElementById("closed").innerHTML = closedmessage;
+
+   
+}, function (errorObject) {
+  console.log("The read failed: " + errorObject.code);
+});
+
+
+	
 			
 });
-			
